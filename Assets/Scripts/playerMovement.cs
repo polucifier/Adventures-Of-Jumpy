304b8dc8d21f14b1771c59627c5d9e31867a621c
polucifier <misha.naughty.boy@gmail.com>
2022-09-10 17:07:49 +0200

    fix for jumps and walk sticking
    it is no more possible to jump multiple times and stick to the walls when holding a move button.

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -7,24 +7,31 @@ public class playerMovement : MonoBehaviour
     [SerializeField] private float jumpForce;
     private Rigidbody2D rb;
     private float direction;
-    private void Start()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(direction * speed, rb.velocity.y);
     }
     private void OnMove(InputValue movementValue)
     {
         direction = movementValue.Get<float>();
-        if (direction != 0) //flipping left-right
+        if(direction != 0) //flipping left-right
         {
             transform.localScale = new Vector3(6.25f * direction, 6.25f, 1f);
         }
     }
     private void OnJump()
     {
-        rb.AddForce(Vector2.up * jumpForce);
+        if(isGrounded())
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+        }
     }
-    private void FixedUpdate()
+    private bool isGrounded()
     {
-        rb.velocity = new Vector2(direction * speed, rb.velocity.y);
+        return Physics2D.BoxCast(GetComponent<BoxCollider2D>().bounds.center, GetComponent<BoxCollider2D>().bounds.size, 0, Vector2.down, 0.1f).collider != null;
     }
 }
