f724b191420f5b6d03c2f1103398186008eb2cb0
polucifier <misha.naughty.boy@gmail.com>
2022-09-23 23:13:49 +0200

    isGrounded+softbody improvements

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -3,14 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class movement : MonoBehaviour
 {
-    private Rigidbody2D rb;
     [SerializeField] private Transform pointer;
     [SerializeField] private float force;
+    [SerializeField] private Rigidbody2D[] rigidbodies2D = new Rigidbody2D[5];
     private Vector2 dirVec;
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
     private void FixedUpdate()
     {
         GetDirection();
@@ -18,9 +14,13 @@ public class movement : MonoBehaviour
         {
             SceneManager.LoadScene(0);
         }
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetButtonDown("Fire1") && isGrounded())
         {
-            rb.AddForce(dirVec * force);
+            //rigidbodies2D[0].AddForce(dirVec * force);
+            foreach(Rigidbody2D rb in rigidbodies2D)
+            {
+                rb.AddForce(dirVec * force);
+            }
         }
     }
     private void GetDirection()
@@ -31,17 +31,21 @@ public class movement : MonoBehaviour
         if(angle > 0f)
         {
             pointer.rotation = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
-            pointer.position = rb.position + dirVec;
+            pointer.position =  rigidbodies2D[0].position + dirVec;
         }
         else if(angle > -90f)
         {
             pointer.rotation = Quaternion.AngleAxis(-90f, Vector3.forward);
-            pointer.position = rb.position + new Vector2(1f, 0f);
+            pointer.position =  rigidbodies2D[0].position + new Vector2(1f, 0f);
         }
         else
         {
             pointer.rotation = Quaternion.AngleAxis(90f, Vector3.forward);
-            pointer.position = rb.position + new Vector2(-1f, 0f);
+            pointer.position =  rigidbodies2D[0].position + new Vector2(-1f, 0f);
         }
+    }
+    private bool isGrounded()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, 0.5f, LayerMask.GetMask("obstacles"));
     }
 }
